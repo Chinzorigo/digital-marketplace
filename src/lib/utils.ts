31040e6a6dfee6575e6d76fe77dd756d4a86a260
Chinzorigo <chinzorig.o@gmail.com>
2023-12-11 21:41:55 +0800

    added shopping card

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,25 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatPrice(
+  price: number | string,
+  options: {
+    currency?: "USD" | "EUR" | "GBP" | "JPY" | "CNY" | "KRW" | "CAD" | "AUD";
+    notation?: Intl.NumberFormatOptions["notation"];
+  } = {}
+) {
+  const { currency = "USD", notation = "compact" } = options;
+
+  const numericPrice = typeof price === "string" ? parseInt(price, 10) : price;
+
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: currency,
+    notation: notation,
+    maximumFractionDigits: 2,
+  }).format(numericPrice);
 }
