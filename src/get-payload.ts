e6f88d1b338c13dbe78c246f112f35730c7d88ac
Chinzorigo <chinzorig.o@gmail.com>
2023-12-12 00:09:08 +0800

    setup admin panel with payload

---
+++ src/get-payload.ts
@@ -0,0 +1,47 @@
+import dotenv from "dotenv";
+import path from "path";
+import { InitOptions } from "payload/config";
+import payload from "payload";
+
+dotenv.config({
+  path: path.resolve(__dirname, "../.env"),
+});
+
+let cachedPayload = (global as any).payload;
+if (!cachedPayload) {
+  cachedPayload = (global as any).payload = {
+    client: null,
+    promise: null,
+  };
+}
+
+interface Args {
+  initOptions?: Partial<InitOptions>;
+}
+
+export const getPayloadClient = async ({ initOptions }: Args = {}) => {
+  if (!process.env.PAYLOAD_SECRET) {
+    throw new Error("PAYLOAD_SECRET is not defined");
+  }
+
+  if (cachedPayload.client) {
+    return cachedPayload.client;
+  }
+
+  if (!cachedPayload.promise) {
+    cachedPayload.promise = payload.init({
+      secret: process.env.PAYLOAD_SECRET,
+      local: initOptions?.express ? false : true,
+      ...(initOptions || {}),
+    });
+  }
+
+  try {
+    cachedPayload.client = await cachedPayload.promise;
+  } catch (e: unknown) {
+    cachedPayload.promise = null;
+    throw e;
+  }
+
+  return cachedPayload.client;
+};
